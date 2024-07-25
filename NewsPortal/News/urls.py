3b101be98631378fcd64f5dea725d97ccb9ce64d
Irina-lol <irinakazynkina@gmail.com>
2024-07-25 10:24:03 +0700

    Update urls.py

--- NewsPortal/News/urls.py
+++ NewsPortal/News/urls.py
@@ -3,11 +3,13 @@ from .views import PostsList, PostDetail, PostsSearch, NewsCreate, ArticlesCreat
 
 
 urlpatterns = [
-   path('<int:pk>', PostsList.as_view(), name='news'),
+   path('', PostsList.as_view(), name='post_list'),
    path('<int:pk>', PostDetail.as_view(), name='newss'),
    path('search/', PostsSearch.as_view(), name='post_search'),
-   path('news/create/', NewsCreate.as_view(), name='news_create'),
-   path('articles/create/', ArticlesCreate.as_view(), name='articles_create'),
-   path('<int:pk>/news/delete/', NewsDelete.as_view(), name='news_delete'),
-   path('<int:pk>/articles/delete/', ArticlesDelete.as_view(), name='articles_delete'),
+   path('news/create/', NewsCreate.as_view(), name='post_edit'),
+   path('news/<int:pk>/edit/',NewsCreate.as_view(), name='post_edit'),
+   path('news/<int:pk>/delete/', NewsDelete.as_view(), name='post_delete'),
+   path('articles/create/', ArticlesCreate.as_view(), name='arpost_edit'),
+   path('articles/<int:pk>/edit/', ArticlesCreate.as_view(), name='arpost_edit'),
+   path('articles/<int:pk>/delete/', ArticlesDelete.as_view(), name='post_delete'),
 ]
