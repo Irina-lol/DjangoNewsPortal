9e94d28e62a093213c29a65c92a5415d8704ae08
Irina-lol <irinakazynkina@gmail.com>
2024-07-25 10:23:36 +0700

    Update views.py

--- NewsPortal/News/views.py
+++ NewsPortal/News/views.py
@@ -34,33 +34,76 @@ class PostsSearch(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         self.filterset = PostFilter(self.request.GET, queryset)
-        return self.filterset.qs
+        return self.filterset.qsfrom django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from .filters import PostFilter
+from .models import Post
+from .forms import NewsForm, ArticlesForm
+
+class PostsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    ordering = '-date_in'
+    paginate_by = 10
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['filterset'] = self.filterset
+        context['news_count'] = self.get_queryset().count()
         return context
 
 
-class NewsCreate(CreateView):
-    form_class = NewsForm
+
+class PostDetail(DetailView):
     model = Post
-    template_name = 'news_create.html'
+    template_name = 'newss.html'
+    context_object_name = 'newss'
 
 
-class ArticlesCreate(CreateView):
-    form_class = ArticlesForm
+class PostsSearch(ListView):
     model = Post
-    template_name = 'articles_create.html'
+    template_name = 'post_search.html'
+    context_object_name = 'news'
+    paginate_by = 10
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qsfrom django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from .filters import PostFilter
+from .models import Post
+from .forms import NewsForm, ArticlesForm
 
-class NewsDelete(DeleteView):
+class PostsList(ListView):
     model = Post
-    template_name = 'news_delete.html'
-    success_url = reverse_lazy('news.html')
+    template_name = 'news.html'
+    context_object_name = 'news'
+    ordering = '-date_in'
+    paginate_by = 10
 
 
-class ArticlesDelete(DeleteView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['news_count'] = self.get_queryset().count()
+        return context
+
+
+
+class PostDetail(DetailView):
     model = Post
-    template_name = 'articles_delete.html'
-    success_url = reverse_lazy('news.html')
+    template_name = 'newss.html'
+    context_object_name = 'newss'
+
+
+class PostsSearch(ListView):
+    model = Post
+    template_name = 'post_search.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
