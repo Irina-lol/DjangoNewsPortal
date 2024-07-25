67f93980235ac7572c45e1820cdf13b7cf1701bf
Irina-lol <irinakazynkina@gmail.com>
2024-07-25 17:23:01 +0700

    Update urls.py

--- NewsPortal/News/urls.py
+++ NewsPortal/News/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from .views import PostsList, PostDetail, PostsSearch, NewsCreate, ArticlesCreate, NewsDelete, ArticlesDelete
+from .views import PostsList, PostDetail, PostsSearch, PostCreate, PostDelete
 
 
 urlpatterns = [
    path('', PostsList.as_view(), name='post_list'),
    path('<int:pk>', PostDetail.as_view(), name='newss'),
    path('search/', PostsSearch.as_view(), name='post_search'),
-   path('news/create/', NewsCreate.as_view(), name='post_edit'),
-   path('news/<int:pk>/edit/',NewsCreate.as_view(), name='post_edit'),
-   path('news/<int:pk>/delete/', NewsDelete.as_view(), name='post_delete'),
-   path('articles/create/', ArticlesCreate.as_view(), name='arpost_edit'),
-   path('articles/<int:pk>/edit/', ArticlesCreate.as_view(), name='arpost_edit'),
-   path('articles/<int:pk>/delete/', ArticlesDelete.as_view(), name='post_delete'),
+   path('news/create/', PostCreate.as_view(), name='post_edit'),
+   path('news/<int:pk>/edit/', PostCreate.as_view(), name='post_edit'),
+   path('news/<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
+   path('articles/create/', PostCreate.as_view(), name='post_edit'),
+   path('articles/<int:pk>/edit/', PostCreate.as_view(), name='post_edit'),
+   path('articles/<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
 ]
